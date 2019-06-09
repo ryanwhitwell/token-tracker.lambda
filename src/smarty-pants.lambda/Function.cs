f5244bfcd7d8e8912a0aa0d17b03e51878b1951b
Ryan Whitwell <ryan.whitwell@wexinc.com>
2019-06-09 16:21:50 -0500

    Added support fo JSON and Alexa skill input.

--- src/smarty-pants.lambda/Function.cs
+++ src/smarty-pants.lambda/Function.cs
@@ -2,26 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 using Amazon.Lambda.Core;
+using Slight.Alexa.Framework.Models.Requests;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace smarty_pants.lambda
 {
-    public class Function
+  public class Function
+  {
+    public string FunctionHandler(SkillRequest input, ILambdaContext ILambdaContext)
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
-        {
-            return input?.ToUpper();
-        }
+      this.LogMessage(ILambdaContext, input);
+
+      return "End";
     }
+
+    public void LogMessage(ILambdaContext ctx, SkillRequest input)
+    {
+      ctx.Logger.LogLine(
+          string.Format("{0}:{1} - {2}",
+              ctx.AwsRequestId,
+              ctx.FunctionName,
+              JsonConvert.SerializeObject(input)));
+    }
+  }
 }
--- test/smarty-pants.lambda.Tests/FunctionTest.cs
+++ test/smarty-pants.lambda.Tests/FunctionTest.cs
@@ -8,21 +8,44 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 
 using smarty_pants.lambda;
+using Slight.Alexa.Framework.Models.Requests;
+using Slight.Alexa.Framework.Models.Requests.RequestTypes;
 
 namespace smarty_pants.lambda.Tests
 {
-    public class FunctionTest
+  public class FunctionTest
+  {
+    [Fact]
+    public void TestFunction()
     {
-        [Fact]
-        public void TestToUpperFunction()
-        {
+      Function function = new Function();
+      TestLambdaContext context = new TestLambdaContext();
+      SkillRequest input = new SkillRequest();
+      Intent myIntent = new Intent();
+      myIntent.Name = "CreateRelationship";
+      myIntent.Slots = new Dictionary<string, Slot>()
+      {
+        ["slot1"] = new Slot() { Name = "slot1Name", Value = "slot1Value" }
+      };
 
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new Function();
-            var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
+      RequestBundle myRequestBundle = new RequestBundle();
+      myRequestBundle.Type = "myType";
+      myRequestBundle.RequestId = "myRequestId";
+      myRequestBundle.Timestamp = new DateTime();
+      myRequestBundle.Intent = myIntent;
+      myRequestBundle.Reason = "myReason";
+      input.Request = myRequestBundle;
 
-            Assert.Equal("HELLO WORLD", upperCase);
-        }
+      String expectedResponse = "End";
+      String response = function.FunctionHandler(input, context);
+
+      Assert.Equal(expectedResponse, response);
     }
+
+    [Fact]
+    public void Truthtest()
+    {
+      Assert.True(true);
+    }
+  }
 }
