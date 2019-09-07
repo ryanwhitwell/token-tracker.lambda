b64b70e823a097453784991ae56cfdd87e28d314
Ryan Whitwell <ryan.whitwell@wexinc.com>
2019-09-07 15:11:41 -0500

    Added test code for connecting to dynamoDB

--- token.lambda/Function.cs
+++ token.lambda/Function.cs
@@ -2,6 +2,11 @@ using Alexa.NET;
 using Alexa.NET.Request;
 using Alexa.NET.Response;
 using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon;
+using Amazon.DynamoDBv2.Model;
+using System.Threading.Tasks;
 using System;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -9,7 +14,7 @@ namespace token.lambda
 {
   public class Function
   {
-    public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+    public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
     {
       // build the speech response 
       SsmlOutputSpeech speech = new SsmlOutputSpeech();
@@ -17,7 +22,21 @@ namespace token.lambda
       
       // create the response using the ResponseBuilder
       SkillResponse finalResponse = ResponseBuilder.Tell(speech);
+
+      string tableName = "token";
+      int itemsCount = await this.GetAllItemsCount(tableName);
+
+      Console.WriteLine("Found {0} item(s) in the table '{1}'.", itemsCount, tableName);
+
       return finalResponse;
+    }
+
+    public async Task<int> GetAllItemsCount(string tableName) 
+    {
+      AmazonDynamoDBClient client = new AmazonDynamoDBClient(RegionEndpoint.USEast1);
+      ScanRequest req = new ScanRequest(tableName);
+      ScanResponse resp = await client.ScanAsync(req);
+      return resp.Count;
     }
   }
 }
