34a29fd2004f5f1f9adc9fd56eafd519e2dec659
Ryan Whitwell <ryan.whitwell@wexinc.com>
2019-09-21 13:10:21 -0500

    Added player tests.

---
+++ Tests/PlayerTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Token.Models;
+using Xunit;
+
+namespace Tests
+{
+  public class PlayerTests
+  {
+    [Fact]
+    public void Constructor_GeneratesInstanceOfClass()
+    {
+      string expectedName = "TestUser";
+      int expectedPoints = 3;
+      
+      Player player = new Player();
+      player.Name = expectedName;
+      player.Points = expectedPoints;
+
+      Assert.IsType<Player>(player);
+      Assert.Equal(expectedName, player.Name);
+      Assert.Equal(expectedPoints, player.Points);
+    }
+  }
+}
