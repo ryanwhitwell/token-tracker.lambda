ee415e0a69fa8a088eadf31ac5222c5925daee7d
Ryan Whitwell <ryan.whitwell@wexinc.com>
2019-09-24 16:45:11 -0500

    Added artifact RequestBusinessLogicTests

---
+++ Tests/BusinessLogic/RequestBusinessLogicTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Token.Tests.BusinessLogic
+{
+  public class RequestBusinessLogicTests
+  {
+    [Fact]
+    public void TestFunction()
+    {
+      Assert.Equal("HELLO WORLD", "HELLO WORLD");
+    }
+  }
+}
